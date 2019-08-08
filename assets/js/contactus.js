eb2893c8615afb2da59f722020b7fafa26bf6463
Elsa Gomez <elsa.ghdz@gmail.com>
2019-08-08 16:58:13 -0400

    Add favicons and correct JS

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,10 +1,10 @@
 var config = {
-    apiKey: "AIzaSyBC_RyyJHImTK-UwvIKz_UTLetPyaTXDRQ",
-    authDomain: "dvmcontactus.firebaseapp.com",
-    databaseURL: "https://dvmcontactus.firebaseio.com",
-    projectId: "dvmcontactus",
-    storageBucket: "dvmcontactus.appspot.com",
-    messagingSenderId: "323233305109"
+    apiKey: "AIzaSyAnF9OklELLx-GH4g9gSHPpWe66f7hucKk",
+    authDomain: "portfolio-4d2a3.firebaseapp.com",
+    databaseURL: "https://portfolio-4d2a3.firebaseio.com",
+    projectId: "portfolio-4d2a3",
+    storageBucket: "portfolio-4d2a3.appspot.com",
+    messagingSenderId: "394562979074"
 };
 firebase.initializeApp(config);
 
@@ -12,24 +12,25 @@ var database = firebase.database();
 
 $("#submit").click(function(event) {
     event.preventDefault();
-    var name = $("#name").val().trim();
-    var phone = $("#phone").val().trim();
-    var email = $("#email").val().trim();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
     var message = $("#message").val();
 
 
 
     var newContact = {
         name: name,
-        phone: phone,
         email: email,
-        message: message,
-       
+        phone: phone,
+        message: message
+
 
     }
 
     database.ref().push(newContact);
 
+    document.getElementById("contactus-form").reset();
 
 
 
